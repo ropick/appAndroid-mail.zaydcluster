a90a7f21f3773dd6fb815b3c36ed3e01ca233849
ropick <ropick@users.noreply.github.com>
2026-08-24 11:06:27 +0000

    enable buildConfig for version display

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,6 +21,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
